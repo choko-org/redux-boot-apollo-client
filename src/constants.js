f9e04fd3079045dac3531e46bfeff3b8bafba0c6
Sebastian Ferrari <sebas@taller.net.br>
2016-07-09 17:01:21 -0300

    Removed constants file.

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const HTTP_BOOT = 'redux-boot/module-boilerplate/http-boot'
-export const HTTP_AFTER_BOOT = 'redux-boot/module-boilerplate/http-after-boot'
-export const HTTP_REQUEST = 'redux-boot/module-boilerplate/http-request'
